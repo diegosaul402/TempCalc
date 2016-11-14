0872a1e91abb8fde28a7e0f114171d05c228a2f8
Diego Mendoza <diego_saul_402@hotmail.com>
2016-11-13 18:15:37 -0600

    Move input functions to mainActivity, clear inputFragment

--- app/src/main/java/com/diegosaul402/tempcalc/MainActivity.java
+++ app/src/main/java/com/diegosaul402/tempcalc/MainActivity.java
@@ -1,29 +1,45 @@
 package com.diegosaul402.tempcalc;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-import com.diegosaul402.tempcalc.fragments.DetailsFragment;
 import com.diegosaul402.tempcalc.fragments.DetailsFragmentListener;
-import com.diegosaul402.tempcalc.fragments.inputFragment;
+import com.diegosaul402.tempcalc.temps.TempEntity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.diegosaul402.tempcalc.temps.TempUtils.fromCelsius;
+import static com.diegosaul402.tempcalc.temps.TempUtils.fromFahrenheit;
+import static com.diegosaul402.tempcalc.temps.TempUtils.fromKelvin;
 
 public class MainActivity extends AppCompatActivity {
 
-    public inputFragment inputFragment = null;
-    public DetailsFragment detailsFragment = null;
-
     public DetailsFragmentListener fragmentListener;
+    @Bind(R.id.txtInput)
+    EditText txtInput;
+    @Bind(R.id.radioCelsius)
+    RadioButton radioCelsius;
+    @Bind(R.id.radioFar)
+    RadioButton radioFar;
+    @Bind(R.id.radioKelvin)
+    RadioButton radioKelvin;
+    @Bind(R.id.radioGroup)
+    RadioGroup radioGroup;
+    @Bind(R.id.textOutput)
+    TextView textOutput;
+    @Bind(R.id.btCalculate)
+    Button btCalculate;
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -33,9 +49,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        radioCelsius.setChecked(true);
+        txtInput.setFocusable(true);
     }
 
 
@@ -59,5 +79,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.btCalculate)
+    public void handleClick(){
+        String strInput = txtInput.getText().toString().trim();
+
+        if(!strInput.isEmpty()){
+            double inputD = Double.parseDouble(strInput);
+            TempEntity tempEntity;
+
+            if(radioCelsius.isChecked()) {
+                tempEntity = fromCelsius(inputD);
+                textOutput.setText(tempEntity.getStrOutput());
+            }
+            if(radioFar.isChecked()){
+                tempEntity = fromFahrenheit(inputD);
+                textOutput.setText(tempEntity.getStrOutput());
+            }
+            if(radioKelvin.isChecked()) {
+                tempEntity = fromKelvin(inputD);
+                textOutput.setText(tempEntity.getStrOutput());
+            }
+        }
+        else {
+            txtInput.setError(getString(R.string.Required));
+        }
+
     }
 }
--- app/src/main/java/com/diegosaul402/tempcalc/fragments/inputFragment.java
+++ app/src/main/java/com/diegosaul402/tempcalc/fragments/inputFragment.java
@@ -5,44 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.diegosaul402.tempcalc.R;
-import com.diegosaul402.tempcalc.temps.TempEntity;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-import static com.diegosaul402.tempcalc.temps.TempUtils.fromCelsius;
-import static com.diegosaul402.tempcalc.temps.TempUtils.fromFahrenheit;
-import static com.diegosaul402.tempcalc.temps.TempUtils.fromKelvin;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class inputFragment extends Fragment {
-
-
-    @Bind(R.id.txtInput)
-    EditText textInput;
-    @Bind(R.id.radioCelsius)
-    RadioButton radioCelsius;
-    @Bind(R.id.radioFar)
-    RadioButton radioFar;
-    @Bind(R.id.radioKelvin)
-    RadioButton radioKelvin;
-    @Bind(R.id.radioGroup)
-    RadioGroup radioGroup;
-    @Bind(R.id.textOutput)
-    TextView textOutput;
-    @Bind(R.id.btCalculate)
-    Button btCalculate;
 
     public inputFragment() {
         // Required empty public constructor
@@ -53,44 +21,11 @@ public class inputFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_input, container, false);
-        ButterKnife.bind(this, view);
-        radioCelsius.setChecked(true);
-        textInput.setFocusable(true);
-
         return view;
-    }
-
-    @OnClick(R.id.btCalculate)
-    public void handleClick(){
-        String strInput = textInput.getText().toString().trim();
-
-        if(!strInput.isEmpty()){
-            double inputD = Double.parseDouble(strInput);
-            TempEntity tempEntity;
-
-            if(radioCelsius.isChecked()) {
-                tempEntity = fromCelsius(inputD);
-                textOutput.setText(tempEntity.getStrOutput());
-            }
-            if(radioFar.isChecked()){
-                tempEntity = fromFahrenheit(inputD);
-                textOutput.setText(tempEntity.getStrOutput());
-            }
-            if(radioKelvin.isChecked()) {
-                tempEntity = fromKelvin(inputD);
-                textOutput.setText(tempEntity.getStrOutput());
-            }
-        }
-        else {
-            Toast toast = Toast.makeText(getActivity().getApplicationContext(),getString(R.string.Required), Toast.LENGTH_SHORT);
-            toast.show();
-        }
-
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        ButterKnife.unbind(this);
     }
 }
